61b7fe7ed4453e17ce1096db69dd30a1baa72590
mikewanjero <mikewanj@gmail.com>
2024-06-13 13:08:53 +0300

    Editing navigation configuration

--- App.js
+++ App.js
@@ -6,14 +6,33 @@ import { StyleSheet, Text, View } from "react-native";
 import manageExpense from "./screens/manageExpense";
 import recentExpenses from "./screens/recentExpenses";
 import allExpenses from "./screens/allExpenses";
+import Constants from "./constants/styles";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function expenseOverview() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Recent Expenses" component={recentExpenses} />
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Constants.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: Constants.colors.primary500 },
+        tabBarActiveTintColor: Constants.colors.accent500,
+      }}
+    >
+      <Tab.Screen
+        name="Recent Expenses"
+        component={recentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="hourglass-empty" size={size} color={color} />
+          ),
+        }}
+      />
       <Tab.Screen name="All Expenses" component={allExpenses} />
     </Tab.Navigator>
   );
@@ -26,7 +45,11 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Expenses Overview">
           <Stack.Screen name="Manage Expense" component={manageExpense} />
-          <Stack.Screen name="Expenses Overview" component={expenseOverview} />
+          <Stack.Screen
+            name="Expenses Overview"
+            component={expenseOverview}
+            options={{ headerShown: false }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>

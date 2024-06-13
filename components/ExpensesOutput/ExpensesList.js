636afb9ec074241baac7d0aff80dd590fb58d6f1
mikewanjero <mikewanj@gmail.com>
2024-06-13 13:09:14 +0300

    Creating key app components to display expenses

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,8 @@
+import { FlatList, StyleSheet } from "react-native";
+import React from "react";
+
+export default function ExpensesList() {
+  return <FlatList />;
+}
+
+const styles = StyleSheet.create({});

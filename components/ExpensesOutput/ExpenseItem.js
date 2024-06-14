2db6dccfbe4281981099fbfb459b135a4545848b
mikewanjero <mikewanj@gmail.com>
2024-06-14 16:47:16 +0300

    formatting date and adding 2dp to amount

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,6 +1,7 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Constants from "./../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 export default function ExpenseItem({ title, amount, date }) {
   return (
@@ -8,10 +9,10 @@ export default function ExpenseItem({ title, amount, date }) {
       <View style={styles.item}>
         <View style={styles.textBase}>
           <Text style={[styles.textBase, styles.description]}>{title}</Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -43,6 +44,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: Constants.colors.primary500,

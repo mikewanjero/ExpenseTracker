e591d33f91d858deea74d6a1f03807d47e3c9242
mikewanjero <mikewanj@gmail.com>
2024-07-01 10:06:10 +0300

    adding styling

--- components/ExpenseMgt/Input.js
+++ components/ExpenseMgt/Input.js
@@ -1,10 +1,41 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import Constants from "./../../constants/styles";
 
 export default function Input({ label, textInputConfig }) {
+  // Allowing style array to have more than one style
+  const inputStyles = [styles.input];
+
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput {...textInputConfig} style={styles.input} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: Constants.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    color: Constants.colors.primary700,
+    backgroundColor: Constants.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});

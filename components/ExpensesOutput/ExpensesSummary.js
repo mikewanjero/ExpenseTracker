247015d55d85785c95ed8423e616286c1bf7f1db
mikewanjero <mikewanj@gmail.com>
2024-07-02 13:55:25 +0300

    #

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,5 +1,4 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import Constants from "./../../constants/styles";
 
 export default function ExpensesSummary({ expenses, periodName }) {
